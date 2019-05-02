8af211dabbefe9de72ac8f76e0925ec6e92df907
idrismeribah <38036241+idrismeribah@users.noreply.github.com>
2019-05-02 12:51:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('include/header.php');
+require_once('include/sidebar.php');
+require_once('include/sidebarde.php');
+require_once('include/sidebartrois.php');
+
+echo'<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></br></br></br>';
+require_once('include/footer.php');
+
+
+
+?>
+
